f53de1e3d9c80bc230c7fc1667b0b339b99e7be2
janithrs <janithravindu@gmail.com>
2022-07-05 12:46:50 +0530

    Init next js and tailwind

---
+++ plz/pages/index.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className='flex flex-col items-center justify-center w-full h-screen'>
+    <h1 className='text-3xl'>Instant Pickup Lines</h1>
+  </div>
+  )
+}
